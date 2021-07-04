528e72cc2d5b5ca6c6a6d4c51d8df7c3ae67898f
INT3NSE07 <d.jonathanpaul@gmail.com>
2021-07-04 21:04:36 +0530

    Add initial changes

---
+++ telemetry_settings.php
@@ -0,0 +1,14 @@
+<?php
+
+$db_type="mysql"; //Type of db: "mysql", "sqlite" or "postgresql"
+
+// Sqlite3 settings
+$Sqlite_db_file = "../telemetry.sql";
+
+// Mysql settings
+$MySql_username="root";
+$MySql_password="root";
+$MySql_hostname="localhost";
+$MySql_databasename="mysql";
+
+?>
